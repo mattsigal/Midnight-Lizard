d101cbf207c49e9164e17e21635427a878634b21
Matthew Sigal <matthewsigal@gmail.com>
2025-10-05 14:46:46 -0700

    Delete docker-build.sh
    Shell script had issues and is no longer needed.

--- docker-build.sh
+++
@@ -1,5 +0,0 @@
-# eval $(docker-machine env default --shell bash)
-mkdir releases
-TAG=midnight-lizard:$(date +"%Y-%m-%d--%H-%M-%S")
-docker build -t $TAG .
-docker run --rm -it -v $(pwd -$1)/releases:/build/releases $TAG
